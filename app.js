6b871600ab2ff2df5f66dd80ad6336538923aa0a
andrés <aferrarivalle@gmail.com>
2022-06-22 12:05:14 -0300

    registro responsive

--- app.js
+++ app.js
@@ -8,10 +8,14 @@ const ruteoProducto = require ('./routers/product');
 const ruteoUsuario = require ('./routers/user');
 const ruteoPrincipal = require ('./routers/main');
 const ruteoAdmin = require ('./routers/admin');
+const exp = require("constants");
 
 app.use( express.static (__dirname + "\\public"));
 
 app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.listen(8000,() => {
     console.log("Servidor Corriendo");
