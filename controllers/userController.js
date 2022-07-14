79da9e4e94f0672e00ccfe5113c4a20cdac804fd
YuriGGG <yurijp95@hotmail.com>
2022-07-13 22:11:13 -0500

    NOTAS PARA FORMULARIO REGISTRO YURI

--- controllers/userController.js
+++ controllers/userController.js
@@ -13,4 +13,27 @@ const controladorUsuario = {
 
 };
 
+
+/*IMPLEMENTAR REGISTRO DE USUARIOS YURIANA NOTAS
+
+*SUBIDA FOTO DE PERFIL
+*CONTRASENA
+*ENCRIPTAR CONTRASENA
+*GUARDAR DATOS EN VIADOS EN JASON DE USUARIOS
+
+1. products (GET)
+Listado de productos
+2. /products/create (GET)
+Formulario de creación de productos
+3. /products/:id (GET)
+Detalle de un producto particular
+4. /products (POST)
+Acción de creación (a donde se envía el formulario)
+5. /products/:id/edit (GET)
+Formulario de edición de productos
+6. /products/:id (PUT)
+Acción de edición (a donde se envía el formulario):
+7. /products/:id (DELETE)
+Acción de borrado*/
+
 module.exports = controladorUsuario;
