9664ec7cf86d3b68cf33065988176001176ee47c
KLQUINTERO <quintero.karen@correounivalle.edu.co>
2022-07-18 10:14:49 -0500

    Resolviendo inconvenientes, ya corre de nuevo el servidor

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
 const express = require("express");
 const req = require("express/lib/request");
 const app = express();
-
 const path = require("path");
-
-app.listen(8000,() => {
-    console.log("Servidor Corriendo");
-});
+const methodOverride =  require('method-override');
+const exp = require("constants");
 
 //Ruteo
 const ruteoProducto = require ('./routers/product');
@@ -15,32 +12,28 @@ const ruteoPrincipal = require ('./routers/main');
 const ruteoAdmin = require ('./routers/admin');
 const session = require('express-session'); 
 
+// Registro de las paginas donde ingresan los usuarios
+const logMiddlewares = require ('./middlewares/logMiddlewares');
+
 //Ruteo
 app.use('/products', ruteoProducto);
 app.use('/', ruteoUsuario);
 app.use('/', ruteoPrincipal);
 app.use('/', ruteoAdmin);
 
-const exp = require("constants");
-
 app.use( express.static (__dirname + "\\public"));
-
 app.use(session({secret: "Secreto"}));
-
 app.set('view engine', 'ejs');
-
 app.use(express.urlencoded({ extended: false }));  //obtener informacion al enviar el formulario
 app.use(express.json());
 app.use(methodOverride('_method')); // Pasar poder pisar el method="POST" en el formulario por PUT y DELETE 
- // Y // Pasar poder usar los métodos PUT y DELETE
-
 
 // Registro de las paginas donde ingresan los usuarios
 app.use(logMiddlewares);
 
-// Registro de las paginas donde ingresan los usuarios
-const logMiddlewares = require ('./middlewares/logMiddlewares');
-
+app.listen(8000,() => {
+    console.log("Servidor Corriendo");
+});
 
 app.all('*', (req, res) => {  
     res.status(404).sendFile(__dirname + "/views/404.html");
