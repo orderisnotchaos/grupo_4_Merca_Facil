755f3b9f198045e5e91a2857e1e533ce04b182cf
YuriGGG <yurijp95@hotmail.com>
2022-07-19 20:12:10 -0500

    servidor corriendo/cambio nombre routes

--- app.js
+++ app.js
@@ -1,46 +1,42 @@
 const express = require("express");
 const req = require("express/lib/request");
 const app = express();
-
 const path = require("path");
-
-app.listen(8000,() => {
-    console.log("Servidor Corriendo");
-});
-
-//Ruteo
-const ruteoProducto = require ('./routers/product');
-const ruteoUsuario = require ('./routers/user');
-const ruteoPrincipal = require ('./routers/main');
-const ruteoAdmin = require ('./routers/admin');
-const session = require('express-session'); 
-
-//Ruteo
-app.use('/products', ruteoProducto);
-app.use('/', ruteoUsuario);
-app.use('/', ruteoPrincipal);
-app.use('/', ruteoAdmin);
-
+const methodOverride =  require('method-override');
 const exp = require("constants");
 
-app.use( express.static (__dirname + "\\public"));
+//Ruteo
+const ruteoProducto = require ('./routes/product');
+const ruteoUsuario = require ('./routes/user');
+const ruteoPrincipal = require ('./routes/main');
+const ruteoAdmin = require ('./routes/admin');
 
-app.use(session({secret: "Secreto"}));
+const session = require('express-session'); 
 
-app.set('view engine', 'ejs');
-
-app.use(express.urlencoded({ extended: false }));  //obtener informacion al enviar el formulario
-app.use(express.json());
-app.use(methodOverride('_method')); // Pasar poder pisar el method="POST" en el formulario por PUT y DELETE 
- // Y // Pasar poder usar los métodos PUT y DELETE
-
-
-// Registro de las paginas donde ingresan los usuarios
-app.use(logMiddlewares);
 
 // Registro de las paginas donde ingresan los usuarios
 const logMiddlewares = require ('./middlewares/logMiddlewares');
 
+
+//Ruteo
+app.use('/products', ruteoProducto);
+app.use('/users', ruteoUsuario);
+app.use('/', ruteoPrincipal);
+app.use('/', ruteoAdmin);
+
+app.use( express.static (__dirname + "\\public"));
+app.use(session({secret: "Secreto"}));
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));  //obtener informacion al enviar el formulario
+app.use(express.json());
+app.use(methodOverride('_method')); // Pasar poder pisar el method="POST" en el formulario por PUT y DELETE 
+
+// Registro de las paginas donde ingresan los usuarios
+app.use(logMiddlewares);
+
+app.listen(8000,() => {
+    console.log("Servidor Corriendo");
+});
 
 app.all('*', (req, res) => {  
     res.status(404).sendFile(__dirname + "/views/404.html");
