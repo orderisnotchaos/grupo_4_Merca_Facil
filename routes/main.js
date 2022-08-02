e45797fe2137d1d60af33499143da3028229848c
KLQUINTERO <quintero.karen@correounivalle.edu.co>
2022-08-01 23:03:45 -0500

    Ordenando

--- routes/main.js
+++ routes/main.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const mainController = require ('../controllers/mainController');
 
 router.get ('/', mainController.index);
-router.get ('/contacto', mainController.contacto);
-router.get ('/sobrenosotros', mainController.sobrenosotros);
+router.get ('/contact', mainController.contacto);
+router.get ('/aboutUs', mainController.sobrenosotros);
 router.get ('/logout',mainController.logout);
 module.exports = router;
